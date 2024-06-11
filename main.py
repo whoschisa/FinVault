68dd9905d2c41ff0b274e6ceda431f88c878692a
jisooslvr4evr <dakshkothari7@gmail.com>
2024-06-11 23:42:08 +0400

    commit
    desc

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from datetime import date, timedelta
 from tabulate import tabulate
 import matplotlib.pyplot as plt
 import os
+day = date.today()
+
 
 con = mycon.connect(
     host="localhost",
     user="root",
-    passwd="0000"
+    passwd="0000",
+    database="finvault"
 )
 
 if con.is_connected():
@@ -35,38 +38,68 @@ def signup():
     global username                                                     # This will be the global username
     global doj                                                          # This will be the join date
     while True:
-        printbw("╔═════════════════════════════════════════════╗")
-        printbw("║         Enter your desired username:        ║")
-        printbw("╚═════════════════════════════════════════════╝")
+        printbw(r"╔════════════════════════════════════════════════════════════╗")
+        printbw(r"║                Enter your desired username:                ║")
+        printbw(r"╚════════════════════════════════════════════════════════════╝")
         username = input("INPUT:")
         with open("data.csv", "r") as file:
             read = csv.reader(file)
-            user_row = list(read)
-            items = [row[0] for row in user_row]
+            rows = list(read)
+            items = [row[0] for row in rows]
 
         if username in items:
             time.sleep(0.18)
-            print("╔═════════════════════════════════════════════╗")
-            print("║           Username already exists.          ║")
-            print("╚═════════════════════════════════════════════╝")
+            printbw(r"╔════════════════════════════════════════════════════════════╗")
+            printbw(r"║                    Username already exists.                ║")
+            printbw(r"╚════════════════════════════════════════════════════════════╝")
             continue
         
         if len(username) > 20:
             time.sleep(0.18)
-            print("╔═════════════════════════════════════════════╗")
-            print("║         Username should be of max.          ║")
-            print("║               20 characters                 ║")
-            print("╚═════════════════════════════════════════════╝")
+            printbw(r"╔════════════════════════════════════════════════════════════╗")
+            printbw(r"║                Username should be of max                   ║")
+            printbw(r"║                   20 characters                            ║")
+            printbw(r"╚════════════════════════════════════════════════════════════╝")
             continue
         
         break
-    
+
 
     while True:
         time.sleep(0.18)
-        print("╔═══════════════════════════════════════╗")
-        print("║        Enter a valid password:        ║")
-        print("╚═══════════════════════════════════════╝")
+        printbw(r"╔════════════════════════════════════════════════════════════╗")
+        printbw(r"║                  Enter a valid password:                   ║")
+        printbw(r"╚════════════════════════════════════════════════════════════╝")
+
+        pwd = input(" ")
+
+        time.sleep(0.18)
+        printbw(r"╔══════════════════════════════════════════════════════════════╗")
+        printbw(r"║                      Confirm Password                        ║")
+        printbw(r"╚══════════════════════════════════════════════════════════════╝")
+
+        confirm_pwd = input(" ")
+
+        if pwd != confirm_pwd:
+            time.sleep(0.18)
+            printbw(r"╔══════════════════════════════════════════════════════════════╗")
+            printbw(r"║                   Passwords do not match                     ║")
+            printbw(r"╚══════════════════════════════════════════════════════════════╝")
+            continue
+
+        break
+
+    while True:
+        data = [username, pwd]
+        with open('data.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(data)
+            cur.execute(f"CREATE TABLE `{username}` (account INTEGER primary key, balanc DECIMAL(10,2) DEFAULT 0.00, doj DATE );")
+            con.commit()
+            menu()
+
+            continue
+
 
             # Code in Progress
 
@@ -119,6 +152,10 @@ def welcome():
             print("║ Invalid input. Please choose a number ║")
             print("║             from 1 to 3.              ║")
             print("╚═══════════════════════════════════════╝")
+
+def menu():
+    while True:
+        print("Hi")
 
 
 welcome()
